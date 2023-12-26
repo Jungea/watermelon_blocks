7ae73db8442fe7464d9d5c25a9522f259814f26a
Jungea <jungea2241@gmail.com>
2023-12-26 22:35:32 +0900

    게임 배경 생성

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
+import 'src/watermelon_blocks.dart';
+
 void main() {
-  final game = FlameGame();
+  final game = WatermelonBlocks();
   runApp(GameWidget(game: game));
 }
